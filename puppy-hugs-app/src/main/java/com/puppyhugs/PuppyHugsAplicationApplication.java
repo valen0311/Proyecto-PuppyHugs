2a80d4b386573df596c28fcbd01a009eade23163
valen0311 <vdizquierdo.24@est.ucab.edu.ve>
2025-11-08 00:06:18 -0400

    primer commit

---
+++ puppy-hugs-app/src/main/java/com/puppyhugs/PuppyHugsAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.puppyhugs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppyHugsAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppyHugsAplicationApplication.class, args);
+	}
+
+}
---
+++ puppy-hugs-app/src/test/java/com/puppyhugs/PuppyHugsAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puppyhugs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppyHugsAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
