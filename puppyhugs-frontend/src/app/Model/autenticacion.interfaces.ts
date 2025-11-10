1d177e51fe630d65c81aa9762430fd76d07f1d0f
valen0311 <vdizquierdo.24@est.ucab.edu.ve>
2025-11-10 12:11:35 -0400

    Modificación del front

--- puppyhugs-frontend/src/app/Model/autenticacion.interfaces.ts
+++
@@ -1,7 +0,0 @@
-// Archivo: src/app/model/autenticacion.interfaces.ts
-// (Define el DTO o JSON que ENVIAMOS al backend para hacer login)
-
-export interface LoginRequest {
-  correo: string;
-  password: string;
-}
--- puppyhugs-frontend/src/app/Model/carrito.interface.ts
+++
@@ -1,9 +0,0 @@
-// Archivo: src/app/model/carrito.interface.ts
-
-import { Producto } from './producto.interface';
-
-// Un item en el carrito (un producto + la cantidad elegida)
-export interface ItemCarrito {
-  producto: Producto;
-  cantidad: number;
-}
