9c477a0c6d614be915bb55f65b3bd4ea9018b174
jhossbe <jmbello.24@est.ucab.edu.ve>
2025-11-09 22:38:48 -0400

    a

--- puppyhugs-frontend/src/app/app-routing.module.ts
+++ puppyhugs-frontend/src/app/app-routing.module.ts
@@ -1,16 +1,27 @@
-import { guardiaAdmin } from './services/autenticacion.guard';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+// 1. Importa los componentes que vas a usar como "páginas"
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+// (Aquí importarás el dashboard-admin, tienda-cliente, etc.)
+
+// 2. Define las rutas
 const routes: Routes = [
+  // Ruta por defecto (si solo ponen localhost:4200)
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+
+  // Ruta para el Login
   { path: 'login', component: LoginComponent },
-  {
-    path: 'dashboard-admin',
-    component: DashboardAdminComponent,
-    canActivate: [guardiaAdmin] // <-- ¡Aquí se usa el guardia!
-  },
-  {
-    path: 'productos-admin',
-    component: ProductosAdminComponent,
-    canActivate: [guardiaAdmin] // <-- ¡Protegido!
-  }
-  // ...
+
+  // ¡ESTA ES LA RUTA QUE NECESITAS!
+  { path: 'register', component: RegisterComponent },
+
+  // (Aquí irán las rutas de /dashboard-admin, /tienda-cliente, etc.)
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
