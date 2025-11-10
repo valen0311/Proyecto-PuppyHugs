a74c14ce9effa98c35a568599a94f42f43081ddf
Nadi2023 <nibuiz.24@est.ucab.edu.ve>
2025-11-10 00:12:43 -0400

    actualizacion html,css,ts

--- puppyhugs-frontend/src/app/sidebard-admin/sidebard-admin.component.ts
+++ puppyhugs-frontend/src/app/sidebard-admin/sidebard-admin.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AutenticacionService } from '../services/autenticacion.service';
+import { Cliente } from '../model/cliente.interface';
 
 @Component({
-  selector: 'app-sidebard-admin',
-  standalone: true,
-  imports: [],
-  templateUrl: './sidebard-admin.component.html',
-  styleUrl: './sidebard-admin.component.css'
+  selector: 'app-sidebar-admin',
+  templateUrl: './sidebar-admin.component.html',
+  styleUrls: ['./sidebar-admin.component.css']
 })
-export class SidebardAdminComponent {
+export class SidebarAdminComponent implements OnInit {
 
+  // Variable para guardar el usuario y mostrar su nombre
+  usuarioLogueado: Cliente | null = null;
+
+  constructor(private autenticacionService: AutenticacionService) { }
+
+  ngOnInit(): void {
+    // 1. Obtenemos el usuario actual del servicio
+    // (Usamos .value para obtener la foto instantánea de quién está logueado)
+    this.usuarioLogueado = this.autenticacionService.usuarioActualValor;
+  }
+
+  /**
+   * 2. Llama al método logout() del servicio
+   */
+  cerrarSesion(): void {
+    this.autenticacionService.logout();
+  }
 }
