439f8e33b52bd348a3c9e2632d5fd6de738e0138
Nadi2023 <nibuiz.24@est.ucab.edu.ve>
2025-11-10 09:36:31 -0400

    actualizacion pagos

--- puppyhugs-frontend/src/app/services/venta.service.ts
+++ puppyhugs-frontend/src/app/services/venta.service.ts
@@ -1,9 +1,8 @@
-// Archivo: src/app/services/venta.service.ts
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Venta } from '../model/venta.interface'; // Importamos el modelo
+import { Venta } from '../model/venta.interface';
+import { VentaRegistroDTO } from '../model/pago.interface'; // <<< IMPORTACIÓN
 
 @Injectable({
   providedIn: 'root'
@@ -17,12 +16,20 @@ export class VentaService {
 
   /**
    * Conecta con: VentaController -> @GetMapping
-   * (HU-13: Reporte de Ventas)
-   * Obtiene la lista completa de ventas.
+   * Obtiene la lista completa de ventas (para el admin).
    */
   getVentas(): Observable<Venta[]> {
     return this.http.get<Venta[]>(this.apiUrl);
   }
 
-  // (Nota: En el futuro, aquí iría el servicio de 'procesarVenta' del cliente)
+  /**
+   * Conecta con: VentaController -> @PostMapping
+   * Registra una nueva venta desde el flujo del cliente (HU-11).
+   * @param ventaDto Los datos de la venta a registrar.
+   */
+  procesarVenta(ventaDto: VentaRegistroDTO): Observable<Venta> { // <<< NUEVO MÉTODO
+    // El backend recibe el DTO y registra la venta, los detalles y la asocia al cliente.
+    console.log("Enviando DTO de Venta al backend:", ventaDto);
+    return this.http.post<Venta>(this.apiUrl, ventaDto);
+  }
 }
