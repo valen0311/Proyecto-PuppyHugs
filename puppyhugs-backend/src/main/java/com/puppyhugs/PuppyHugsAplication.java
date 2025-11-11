0e4674c6d6b0872672241ab4e12b577cbc6a8788
paola <pvzambrano.24@est.ucab.edu.ve>
2025-11-10 20:27:52 -0400

    merge

--- puppyhugs-backend/src/main/java/com/puppyhugs/PuppyHugsAplication.java
+++ puppyhugs-backend/src/main/java/com/puppyhugs/PuppyHugsAplication.java
@@ -14,7 +14,8 @@ public class PuppyHugsAplication {
    * El método 'main' que sirve como punto de entrada para
    * ejecutar la aplicación.
    */
-  public static void main(String[] args) {
+  public static void
+  main(String[] args) {
     // Esta línea inicia todo el framework de Spring
     SpringApplication.run(PuppyHugsAplication.class, args);
   }
